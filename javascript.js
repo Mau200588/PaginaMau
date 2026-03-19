ac95a7d5092a72fc0aea33ecbe54fa3ace65da1c
Mauricio Calderon <juegosmau.6826@gmail.com>
2026-03-18 22:57:18 -0400

    se agrega mas ejemplos de  javascript

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
-window.onload = function(){
+window.onload = function() {
     alert("tenemos variedad de platos pide el tuyo");
     alert("Menu: Silpancho Pique Macho y Saice");
 };
-document.querySelector('a[href=#sobre-nosotros]').addEventListener('click', function()){
-    alert("hizo click en el evento")
-};
+
+document.querySelector('a[href="#sobre-nosotros"]').addEventListener('click', function(){
+    alert("hizo click en el evento");
+    document.body.style.backgroundColor ="#30302f";
+}); 
+document.querySelectorAll('.btn-primary').forEach(function(btn){
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        const producto = btn.closest('.card').querySelector('.card-title').textContent;
+        alert("solicitando informacion sobre: " + producto);
+    });
+});
+document.querySelectorAll('.btn-warning').forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        e.preventDefault();
+        alert('gracias por su preferencia en comprar el producto')
+    });
+});
