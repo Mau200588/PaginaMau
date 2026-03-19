2d6d7bb8d40f33c17044043e9388ff58a807b536
Mauricio Calderon <juegosmau.6826@gmail.com>
2026-03-18 21:59:18 -0400

    añadiendo javascript

---
+++ javascript.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    alert("tenemos variedad de platos pide el tuyo");
+    alert("Menu: Silpancho Pique Macho y Saice");
+};
+document.querySelector('a[href=#sobre-nosotros]').addEventListener('click', function()){
+    alert("hizo click en el evento")
+};
